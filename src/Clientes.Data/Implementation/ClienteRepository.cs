d0cf766366a23aa1cb4a3427119ad390eeb8acd7
Rodrigo Amaro <jbravo.br@gmail.com>
2017-07-18 22:37:40 -0300

    Merge Manual

--- src/Clientes.Data/Implementation/ClienteRepository.cs
+++ src/Clientes.Data/Implementation/ClienteRepository.cs
@@ -4,15 +4,19 @@ using Clientes.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+<<<<<<< HEAD
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Threading.Tasks;
 using System.Linq;
+=======
+>>>>>>> 06565dc... Container de injeção de dependencia
 
 namespace Clientes.Data.Implementation
 {
     public class ClienteRepository : IRepository<Cliente>
     {
+<<<<<<< HEAD
         IMongoDatabase DbContext { get; }
         string typeName = "BsonDocument";
 
@@ -93,6 +97,48 @@ namespace Clientes.Data.Implementation
             {
                 throw ex;
             }
+=======
+        IRepository<Cliente> _repositorio;
+        public ClienteRepository(IRepository<Cliente> repository)
+        {
+            _repositorio = repository;
+        }
+
+
+        void IRepository<Cliente>.Adicionar(Cliente obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<Cliente>.Atualizar(Cliente obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Cliente> IRepository<Cliente>.Buscar(Expression<Func<Cliente, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        Cliente IRepository<Cliente>.ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<Cliente>.Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IRepository<Cliente>.SaveChanges()
+        {
+            throw new NotImplementedException();
+>>>>>>> 06565dc... Container de injeção de dependencia
         }
     }
 }
---
+++ src/CrossCutting.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CrossCutting.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+
+            services.AddScoped<IRepository<Cliente>,ClienteRepository>();
+           
+        }
+    }
+}
