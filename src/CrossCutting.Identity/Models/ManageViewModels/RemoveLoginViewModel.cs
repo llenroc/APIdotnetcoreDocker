b2264c24168aecc903c506358484138f24341539
PAULO EDUARDO CORREIA <correia97@gmail.com>
2017-07-18 23:47:51 -0300

    Inicio do Identity

---
+++ src/CrossCutting.Identity/Models/ManageViewModels/RemoveLoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ErossCutting.Identity.Models.ManageViewModels
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
---
+++ src/CrossCutting.IoC/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CrossCutting.IoC
+{
+    public class Class1
+    {
+    }
+}
